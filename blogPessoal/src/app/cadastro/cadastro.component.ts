e7aefae51baa963592b0ed5189e956df765bc9dd
unknown <dias.s.fernand1@gmail.com>
2020-12-04 22:14:12 -0300

    add componente login e componente cadastro

--- blogPessoal/src/app/cadastro/cadastro.component.ts
+++ blogPessoal/src/app/cadastro/cadastro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor() { }
+  user: Usuario = new Usuario();
+  senha: string;
 
-  ngOnInit(): void {
+  constructor(
+    private authService: AuthService
+    ) { }
+
+  ngOnInit() {
+  }
+  
+  cadastrar(){
+    this.authService.cadastrar(this.user).subscribe((resp: any = Usuario) => {
+      this.user = resp
+      console.log("usuario usuario" + this.user.nome)
+      console.log("usuario usuario" + this.user.usuario)
+      console.log("usuario senha" + this.user.senha)
+      console.log("senha variavel"+ this.senha)
+    if(this.senha === this.user.senha){
+     this.authService.cadastrar(this.user).subscribe((resp: any = Usuario) =>{
+      this.user = resp
+      this.router.navigate(['/login'])
+      alert('Usuário cadastrado com sucesso!!')
+     })
+    }else{
+      alert('A senha não confere!')
+    }
+    })
   }
 
 }
